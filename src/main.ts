017f153df59e26b9eeed063c46607763bc6b6207
sachanashutosh15 <sachanashutosh15@gmail.com>
2024-10-19 09:51:29 +0530

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   dotenv.config();
   console.log("Port", process.env.PORT);
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: ["https://ecommerce-client-opal.vercel.app"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  })
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
